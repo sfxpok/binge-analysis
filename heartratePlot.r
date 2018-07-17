96fd640e41485c5f2efb79e5e12282f6217163c2
sfxpok <ricardo@airmail.cc>
2018-07-17 19:23:58 +0100

    added code used to plot heartrate data

---
+++ heartratePlot.r
@@ -0,0 +1,13 @@
+library(ggplot2)
+
+hrData_1335_mean$time <- gsub('.{4}$', '', hrData_1335_mean$time)
+hrData_1335_mean["dateTime"] <- as.POSIXct(paste("2017-07-29", hrData_1335_mean$time), format="%Y-%m-%d %H:%M:%S")
+hrData_1335_mean$hr <- as.numeric(levels(hrData_1335_mean$hr))[hrData_1335_mean$hr]
+
+ggplot(hrData_1335_mean, aes(x=dateTime, y=hr, group=1)) +
+  geom_line(size = 0.5, colour = "red") +
+  ggtitle("Heartrate") +
+  labs(x = "Time", y = "BPM") +
+  scale_x_datetime(breaks = date_breaks("15 min"), minor_breaks=date_breaks("15 min"), labels=date_format("%H:%M")) + 
+  scale_y_continuous(limits = c(65, 100)) +
+  theme_bw()
