407a0663b4529cadaf0bc5adf9c45645398962e1
sfxpok <ricardo@airmail.cc>
2018-07-17 19:45:53 +0100

    added some documentation

--- heartratePlot.r
+++ heartratePlot.r
@@ -1,8 +1,8 @@
 library(ggplot2)
 
-hrData_1335_mean$time <- gsub('.{4}$', '', hrData_1335_mean$time)
-hrData_1335_mean["dateTime"] <- as.POSIXct(paste("2017-07-29", hrData_1335_mean$time), format="%Y-%m-%d %H:%M:%S")
-hrData_1335_mean$hr <- as.numeric(levels(hrData_1335_mean$hr))[hrData_1335_mean$hr]
+hrData_1335_mean$time <- gsub('.{4}$', '', hrData_1335_mean$time) # remove miliseconds
+hrData_1335_mean["dateTime"] <- as.POSIXct(paste("2017-07-29", hrData_1335_mean$time), format="%Y-%m-%d %H:%M:%S") # create a date/time POSIXct column
+hrData_1335_mean$hr <- as.numeric(levels(hrData_1335_mean$hr))[hrData_1335_mean$hr] # convert factor to numeric datatype
 
 ggplot(hrData_1335_mean, aes(x=dateTime, y=hr, group=1)) +
   geom_line(size = 0.5, colour = "red") +
