806dc4aea3aca068cb90f712b448782a4fb94a4c
sfxpok <sfxpok@users.noreply.github.com>
2018-07-25 00:52:50 +0100

    adjusted gyro plotting code andde and added accelerometer plotting code

--- accelPlot.r
+++ accelPlot.r
@@ -5,19 +5,19 @@ generalData_1335_mean$time <- gsub('.{4}$', '', generalData_1335_mean$time) # re
 generalData_1335_mean["dateTime"] <- as.POSIXct(paste("2017-07-29", generalData_1335_mean$time), format="%Y-%m-%d %H:%M:%S") # create a date/time POSIXct column
 generalData_1335_mean$hr <- as.numeric(levels(generalData_1335_mean$hr))[generalData_1335_mean$hr] # convert factor to numeric datatype
 
-generalData_1335_mean$gyro.x <-as.numeric(levels(generalData_1335_mean$gyro.x)[generalData_1335_mean$gyro.x]) # convert factor to numeric datatype
-generalData_1335_mean$gyro.y <-as.numeric(levels(generalData_1335_mean$gyro.y)[generalData_1335_mean$gyro.y]) # convert factor to numeric datatype
-generalData_1335_mean$gyro.z <-as.numeric(levels(generalData_1335_mean$gyro.z)[generalData_1335_mean$gyro.z]) # convert factor to numeric datatype
+generalData_1335_mean$accel.x <-as.numeric(levels(generalData_1335_mean$accel.x)[generalData_1335_mean$accel.x]) # convert factor to numeric datatype
+generalData_1335_mean$accel.y <-as.numeric(levels(generalData_1335_mean$accel.y)[generalData_1335_mean$accel.y]) # convert factor to numeric datatype
+generalData_1335_mean$accel.z <-as.numeric(levels(generalData_1335_mean$accel.z)[generalData_1335_mean$accel.z]) # convert factor to numeric datatype
 
 ggplot(generalData_1335_mean, aes(x=dateTime, group=1)) +
-  geom_line(data=generalData_1335_mean, aes(y=gyro.x, color="x"), size = 0.5) +
-  geom_line(data=generalData_1335_mean, aes(y=gyro.y, color="y"), size = 0.5) +
-  geom_line(data=generalData_1335_mean, aes(y=gyro.z, color="z"), size = 0.5) +
-  ggtitle("Gyroscope") +
+  geom_line(data=generalData_1335_mean, aes(y=accel.x, color="x"), size = 0.5) +
+  geom_line(data=generalData_1335_mean, aes(y=accel.y, color="y"), size = 0.5) +
+  geom_line(data=generalData_1335_mean, aes(y=accel.z, color="z"), size = 0.5) +
+  ggtitle("Accelerometer") +
   labs(x = "Time") +
   scale_x_datetime(breaks = date_breaks("15 min"), minor_breaks=date_breaks("15 min"), labels=date_format("%H:%M")) + 
   # scale_y_continuous(limits = c(65, 100)) +
-  scale_y_continuous(limits = c(-60, 60)) + 
+  scale_y_continuous(limits = c(-10, 10)) + 
   scale_color_manual(values = c("blue","red","black")) +
   theme_bw() + 
   theme(axis.title.y=element_blank(), legend.title=element_blank())
